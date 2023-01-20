aa019ea35a784c9e59405fe8cc2f0dec25d7bd90
SafteyDav3 <talktomedw@gmail.com>
2023-01-19 20:30:51 -0600

    components added and selected in the pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 
+import { Product, FooterBanner, HeroBanner } from "../components";
+
 const index = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
+
       <div className="products-heading">
-        <h2 >Best Selling Products</h2>
+        <h2>Best Selling Products</h2>
         <p>Speakers Dat PLOP</p>
       </div>
-
       <div className="products-container">
         {["Product 1", "Product 2"].map((product) => product)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   );
 };
