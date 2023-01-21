70908f5780bc9938b9e49604ffacf1e66491f46d
SafteyDav3 <talktomedw@gmail.com>
2023-01-20 18:39:30 -0600

    Client issues for sanity figued out / adding product props to from sanity to elements

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 
+import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const index = () => {
+const Home = ({ products, bannerData}) => {
   return (
     <>
       <HeroBanner />
+      {console.log(bannerData)}
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers Dat PLOP</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
@@ -20,4 +22,16 @@ const index = () => {
   );
 };
 
-export default index;
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  const bannerQuery = '*[_type == "product"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
+export default Home;
