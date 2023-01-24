1c6e65dd04a59004ebdd46294350464fdeb2c88f
SafteyDav3 <talktomedw@gmail.com>
2023-01-23 22:17:04 -0600

    layout applied and products added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const Layout = () => {
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>NFT Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { Layout } from '../components'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
